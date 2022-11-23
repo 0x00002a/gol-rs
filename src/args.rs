3b30a758439d1a299fb32007dc4bd0477903c84f
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2022-11-23 20:31:09 +0000

    feat: add help strings for args

--- src/args.rs
+++ src/args.rs
@@ -3,11 +3,16 @@ use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 pub struct Args {
+    #[arg(help = "pgm file to read initial state from")]
     pub input: PathBuf,
 
-    #[arg(short, long)]
+    #[arg(short, long, help = "threads to use")]
     pub threads: Option<u16>,
 
-    #[arg(short, long, default_value_t = ' ')]
+    #[arg(
+        long = "bg",
+        default_value_t = ' ',
+        help = "character to use for completely regions"
+    )]
     pub background: char,
 }
